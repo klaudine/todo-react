89b7839df83e45e4116569d8dbe863801ae80fa0
Klaudine <109423321+klaudine@users.noreply.github.com>
2024-01-25 11:56:09 +0000

    made lots of changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,118 @@
 import React, { useState, useEffect } from "react";
-import TaskInput from "./components/TaskInput";
-import Stats from "./components/Stats";
-import ToDoList from "./components/ToDoList";
+// import TaskInput from "./components/TaskInput";
+// import Stats from "./components/Stats";
+// import ToDoList from "./components/ToDoList";
 
-async function createDb() {
-  try {
-    const res = await fetch("https://playground.4geeks.com/apis/fake/todos/user/klaudine", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: [],
-    })
 
-  } catch(err) {
-    console.log(err)
+
+const Home = () => {
+  const [todo, setTodo] = useState('')
+  const [todos, setTodos] = useState([])
+  const [dbExist, setDbExist] = useState(false)
+
+  const handleInput = (evt) => {
+    setTodo(evt.target.value)
   }
-}
 
-createDb()
+  const handleClick =() => {
+    setTodos(prev => [...prev, todo])
+  }
 
-
-function App() {
-  const [toDoList, setToDoList] = useState([]);
-
-useEffect(() => {
-  const getData = async () => {
-    try {
-        const res = await fetch("https://playground.4geeks.com/apis/fake/todos/user/klaudine");
-        const data = await res.json();
-  
-        setToDoList(data)
-        console.log(data);
-  
-      } catch(err) {
-        console.log(err);
-      }
-    }
-
-    getData()
-
-}, [])
-
-
-    const updateData = async (newArray) => {
-      try {
-        const res = await fetch("https://playground.4geeks.com/apis/fake/todos/user/klaudine", {
-        method: "PUT",
+  useEffect (() => {
+    const CreateDb = async () => {
+        const res = await fetch('https://playground.4geeks.com/apis/fake/todos/user/klaudine',
+      {
+        method: 'POST',
+        body: JSON.stringify([]),
         headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newArray),
-        })
-        const data = await res.json();
-        console.log(data);
-
-      } catch(err) {
-        console.log(err)
-      }
-    }
-
-async function deleteUser() {
-  try {
-    const res = await fetch("https://playground.4geeks.com/apis/fake/todos/user/klaudine", {
-      method: "DELETE",
-      headers: {
-          "Content-Type": "application/json"
+          "Content-Type": 'application/JSON'
         }
-    })
-    
-      } catch(err) {
-        console.log(err)
+      })
+      const data = await res.json()
+      if (data.msg === "The user exist") {
+        setDbExist(true)
       }
     }
-  deleteUser();
 
+    CreateDb();
+
+  },[]);
+
+  
+  useEffect (() => {
+    if (dbExist) {
+      const getDb = async () => {
+        const res = await fetch('https://playground.4geeks.com/apis/fake/todos/user/klaudine')
+        const data = await res.json()
+          setTodos(data)
+      }
+      getDb();
+    }
+  },[dbExist])
+
+  const handleDone = (todoId) => () => {
+    const newTodos = todos.map((data) => {
+      if (todoId === data.id) {
+        return {
+          ...data,
+          done: false
+        }
+      }
+      return data
+    })
+    setTodos(newTodos)
+  }
+
+
+  if (!dbExist) {
+    return <div>Loading ...</div>
+  }
 
   return (
-    <>
-    <div className='container'>
-      <h1>Todo List</h1>
-      
-      <TaskInput toDoList={toDoList} setToDoList={setToDoList} updateData={updateData} />
-
-      <div className="toDoList">
-        <span>To do</span>
-        <ToDoList
-        toDoList={toDoList}
-        setToDoList={setToDoList}
-        updateData={updateData}/>
-        {toDoList.length === 0 ?
-        (<p className="notify">
-          All tasks completed!
-        </p>) : null}
+  <div>
+  <input onChange={handleInput} value={todo} />
+  <button onClick={handleClick}>add to list</button>
+    {
+      todos.map(({ done, id, label }) =>
+      <div key={id} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <p
+        key={id}
+        style={{ textDecoration: done ? 'line-through' : 'none' }}
+        >
+          {label}
+        </p>
+        <button onClick={handleDone}>Finished</button>
       </div>
-    </div>
-
-    <Stats toDoList={toDoList} />
-    </>
-  );
+      )
+    }
+  </div>
+  )
 }
 
+//   return (
+//     <>
+//     <div className='container'>
+//       <h1>Todo List</h1>
+      
+//       <TaskInput toDoList={toDoList} setToDoList={setToDoList} updateData={updateData} />
 
-export default App;
+//       <div className="toDoList">
+//         <span>To do</span>
+//         <ToDoList
+//         toDoList={toDoList}
+//         setToDoList={setToDoList}
+//         updateData={updateData}/>
+//         {toDoList.length === 0 ?
+//         (<p className="notify">
+//           All tasks completed!
+//         </p>) : null}
+//       </div>
+//     </div>
+
+//     <Stats toDoList={toDoList} />
+//     </>
+//   );
+// }
+
+
+export default Home;
